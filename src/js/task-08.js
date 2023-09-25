5ae9632e3d08d092f856bf671b28b8f98e56ff59
mariavsl518 <mariavsl518@gmail.com>
2023-09-25 09:41:53 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.login-form');
+
+
+function handlerSubmit(e) { 
+    e.preventDefault();
+    const { email, password } = e.currentTarget.elements;
+    
+}
+
+console.dir(form);
+
+form.addEventListener('submit', handlerSubmit)
 
