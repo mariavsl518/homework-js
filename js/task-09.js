5d96636fbc3498b96e597db066be1b0963f161ba
mariavsl518 <mariavsl518@gmail.com>
2023-09-26 16:20:32 +0200

    complete

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const body = document.querySelector('body')
+
+button.addEventListener('click', colorChanger)
+
+
+function colorChanger() { 
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+}
+
